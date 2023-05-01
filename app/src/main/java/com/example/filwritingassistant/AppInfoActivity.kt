c57876a18d1fd0e49adb3ebc0008dbdac01c2d79
Angelique Joyce Garcia <angeliquejoyceg@gmail.com>
2023-05-01 09:22:20 +0800

    Adding Side Menu to About Activity

--- app/src/main/java/com/example/filwritingassistant/AppInfoActivity.kt
+++ app/src/main/java/com/example/filwritingassistant/AppInfoActivity.kt
@@ -18,10 +18,110 @@ import com.google.firebase.database.ValueEventListener
 
 class AppInfoActivity : AppCompatActivity() {
 
+    //for side menu
+    private lateinit var user : FirebaseAuth
+    lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_info)
 
+        //for side menu
+        val sidemenu = findViewById<ImageView>(R.id.btnSideMenu4)
+        val home = findViewById<LinearLayout>(R.id.home)
+        val profile = findViewById<LinearLayout>(R.id.profile)
+        val aboutUs = findViewById<LinearLayout>(R.id.about)
+        val logout = findViewById<LinearLayout>(R.id.logout)
+        val database = FirebaseDatabase.getInstance().reference
+        val userName = findViewById<TextView>(R.id.tvname)
+        val emailAddress = findViewById<TextView>(R.id.tvemail)
+
+
+        drawer = findViewById(R.id.appinfoparent)
+
+        /** Getting the current user that was logged in to the system **/
+        user = FirebaseAuth.getInstance()
+        val userCurrent = FirebaseAuth.getInstance().currentUser
+
+        /** Getting the current user that was logged in to the system **/
+        if(userCurrent!=null){
+            val email =userCurrent.email
+
+            if (email != null){
+                database.child("Profiles").orderByChild("email").equalTo(email).addListenerForSingleValueEvent(object :
+                    ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        for (profileSnapshot in dataSnapshot.children) {
+                            val profile = profileSnapshot.value as Map<*, *>
+                            val name = profile["name"] as String
+                            val email = profile["email"] as String
+
+                            Log.d("TAG", "Name: $name")
+                            Log.d("TAG", "Email: $email")
+
+                            if (name.isNotEmpty()) {
+                                userName.text = name
+                            }
+
+                            if (email.isNotEmpty()) {
+                                emailAddress.text = email
+                            }
+
+                            runOnUiThread {
+                                userName.text = name
+                                emailAddress.text = email
+                            }
+                            break
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        Toast.makeText(this@AppInfoActivity, "Failed to retrieve data from database", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+        }
+
+        home.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+
+        profile.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+
+        aboutUs.setOnClickListener {
+            startActivity(Intent(this, AppInfoActivity::class.java))
+        }
+
+        logout.setOnClickListener {
+            user.signOut()
+            Toast.makeText(this, "Successfully logged out :)", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        sidemenu.setOnClickListener {
+            openDrawer(drawer)
+        }
+
+
+    }
+
+    fun openDrawer(drawer:DrawerLayout){
+        drawer.openDrawer(GravityCompat.START)
+
+    }
+
+    fun closeDrawer(drawer:DrawerLayout){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onPause(){
+        super.onPause()
+        closeDrawer(drawer)
     }
 
 }
