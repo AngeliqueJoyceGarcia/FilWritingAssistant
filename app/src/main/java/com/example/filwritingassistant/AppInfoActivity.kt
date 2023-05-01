7d884bf1fcf7954f005f2f13bc1e63ff451affd2
Pacion-JessLee <jlbpacion@bpsu.edu.ph>
2023-05-01 09:11:39 +0800

    about
    About

--- app/src/main/java/com/example/filwritingassistant/AppInfoActivity.kt
+++ app/src/main/java/com/example/filwritingassistant/AppInfoActivity.kt
@@ -1,11 +1,27 @@
 package com.example.filwritingassistant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AppInfoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_info)
+
     }
+
 }
