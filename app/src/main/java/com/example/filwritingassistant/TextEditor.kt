6aa7fd23856642ca1c911feab0089d3cac6ef34e
Angelique Joyce Garcia <angeliquejoyceg@gmail.com>
2023-05-04 14:35:58 +0800

    Texteditor Grammar Update
    -3 token re arrangement
    -capital letter for first letter in the sentence
    -proper use of d and r
    -capitalization and period in title
    - spelling suggestion

--- app/src/main/java/com/example/filwritingassistant/TextEditor.kt
+++ app/src/main/java/com/example/filwritingassistant/TextEditor.kt
@@ -6,10 +6,14 @@ import android.content.pm.PackageManager
 import android.media.Image
 import android.os.Bundle
 import android.os.Environment
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
@@ -36,6 +40,27 @@ class TextEditor : AppCompatActivity() {
     private var storageRef = FirebaseStorage.getInstance()
     private lateinit var textEditor: EditText
 
+    /** 1 : For Grammar checking **/
+    private lateinit var wordset: List<String>
+    private lateinit var wordList: List<String>
+
+    private lateinit var expression: List<String>
+    private lateinit var exclamation: List<String>
+    private lateinit var prefix: List<String>
+    private lateinit var interrogative: List<String>
+    private lateinit var verb: List<String>
+    private lateinit var infinitive: List<String>
+    private lateinit var idiomatic: List<String>
+    private lateinit var comparative: List<String>
+    private lateinit var colloquial: List<String>
+    private lateinit var noun: List<String>
+    private lateinit var grammar: List<String>
+    private lateinit var adjective: List<String>
+    private lateinit var preposition: List<String>
+    private lateinit var conjunction: List<String>
+    private lateinit var interjection: List<String>
+    private lateinit var adverb: List<String>
+
     //for side menu
     private lateinit var user : FirebaseAuth
 
@@ -59,6 +84,32 @@ class TextEditor : AppCompatActivity() {
         val userName = findViewById<TextView>(R.id.tvname)
         val emailAddress = findViewById<TextView>(R.id.tvemail)
 
+        /** 1.1 : Continuation of grammar related**/
+        wordset = resources.openRawResource(R.raw.wordset).bufferedReader().readLines()
+        wordList = wordset.map {it.lowercase()}
+
+        expression = resources.openRawResource(R.raw.expression).bufferedReader().readLines()
+        exclamation = resources.openRawResource(R.raw.exclamation).bufferedReader().readLines()
+        prefix = resources.openRawResource(R.raw.prefix).bufferedReader().readLines()
+        interrogative = resources.openRawResource(R.raw.interrogative).bufferedReader().readLines()
+        verb = resources.openRawResource(R.raw.verb).bufferedReader().readLines()
+        infinitive = resources.openRawResource(R.raw.infinitive).bufferedReader().readLines()
+        idiomatic = resources.openRawResource(R.raw.idiomatic).bufferedReader().readLines()
+        comparative = resources.openRawResource(R.raw.comparative).bufferedReader().readLines()
+        colloquial = resources.openRawResource(R.raw.colloquial).bufferedReader().readLines()
+        noun = resources.openRawResource(R.raw.noun).bufferedReader().readLines()
+        grammar = resources.openRawResource(R.raw.grammar).bufferedReader().readLines()
+        adjective = resources.openRawResource(R.raw.adjective).bufferedReader().readLines()
+        preposition = resources.openRawResource(R.raw.preposition).bufferedReader().readLines()
+        conjunction = resources.openRawResource(R.raw.conjunction).bufferedReader().readLines()
+        interjection = resources.openRawResource(R.raw.interjection).bufferedReader().readLines()
+        adverb = resources.openRawResource(R.raw.adverb).bufferedReader().readLines()
+
+        // List of special cases
+        val titles = listOf("mr.", "ms.", "mrs.", "dr.", "dra.", "atty.", "mr", "ms", "mrs", "dr", "dra", "atty")
+        val special_c = listOf("daw", "din", "dito", "diyan", "doon")
+        val special_v = listOf("raw", "rin", "rito", "riyan", "roon")
+        val vowels = listOf("a", "e", "i", "o", "u", "w", "y")
 
         drawer = findViewById(R.id.parenttexteditor)
 
@@ -73,7 +124,7 @@ class TextEditor : AppCompatActivity() {
         val upload = findViewById<ImageView>(R.id.btnupload)
 
         //setting the recognized text to text editor
-       texteditor = findViewById(R.id.et_text_editor)
+        texteditor = findViewById(R.id.et_text_editor)
         val text = intent.getStringExtra("fileContent")
         texteditor.setText(text)
 
@@ -239,9 +290,273 @@ class TextEditor : AppCompatActivity() {
         }
 
         /** Checking the grammar of edit text content **/
+        // suggestion list & text editor variable
+        val suggestions = findViewById<ListView>(R.id.lvsuggestions)
+        texteditor = findViewById(R.id.et_text_editor)
 
+        // Create an empty list to hold suggestions
+        val suggestionList = mutableListOf<String>()
+
+        // Create an adapter for the suggestions list view
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, suggestionList)
+        suggestions.adapter = adapter
+
+        // Add a text change listener to the text editor
+        texteditor.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+
+                // Clear the suggestion list
+                suggestionList.clear()
+
+                // Step 1: Get the text from the editor and split it into sentences
+                val text = s.toString()
+                val sentences = text.split(Regex("(?<=\\.)\\s+"))
+
+                // For handling case 2
+                var prevToken : Any? = null
+
+                for (sentence in sentences) {
+
+                    // Step 1.2: split sentence into tokens
+                    val tokens = sentence.split(Regex("\\s+"))
+
+                    // Step 2.1: Check every sentence if the first word starts with a capital letter
+                    // and is not part of title
+                    if (tokens.first().matches("\\p{Lu}.*".toRegex()) &&
+                        !titles.contains(tokens.first())) {
+
+                        // Step 3: Check every word
+                        for (i in tokens.indices) {
+                            val token = tokens[i]
+
+                            // Case 1: Word is in title list
+                            if (titles.contains(token.lowercase())){
+
+                                // Case 1a: First letter is not capitalized
+                                if (!Character.isUpperCase(token[0])) {
+                                    suggestionList.add("Please capitalize the first letter of the title: $token")
+                                }
+
+                                // Case 1b: Title does not have dot
+                                if (!token.contains(".")) {
+                                    suggestionList.add("Add a period to this title: $token. ")
+                                }
+
+                                // Case 1c: Next word starts with lowercase
+                                if (i < tokens.lastIndex && tokens[i + 1].matches("(\\s)*\\p{Ll}.*".toRegex())) {
+                                    var nextToken = tokens[i + 1][0]
+                                    suggestionList.add("Please capitalize the first letter of : $nextToken")
+                                }
+                            }
+                            // Case 2: Word is in special_c list CONSONANT
+                            else if (special_v.contains(token.lowercase())){
+
+                                // Case 2a: if the last letter of the last token is not in vowels list.
+                                if (prevToken != null && prevToken is String && prevToken.last().toString().lowercase() !in vowels) {
+                                    suggestionList.add("Instead of 'R' use 'D' in the first letter: $token")
+                                }
+                            }
+                            // Case 3: Word is in special_c list CONSONANT
+                            else if (special_c.contains(token.lowercase())){
+
+                                // Case 3a: if the last letter of the last token is in vowels list.
+                                if (prevToken != null && prevToken is String && prevToken.last().toString().lowercase() in vowels) {
+                                    suggestionList.add("Instead of 'D' use 'R' in the first letter: $token")
+                                }
+                            }
+                            // Case 4: Suggesting closest word from word list
+                            else if (!wordList.contains(token.lowercase())){
+                                // Case 4a: Word is not in word list, suggest closest word using Levenshtein distance
+                                val closestWord = getClosestWord(token.lowercase())
+                                if (closestWord != null) {
+                                    val suggestion = "$token | Did you mean: $closestWord"
+                                    suggestionList.add(suggestion)
+                                }
+                            }
+
+                            // Setting up previous token
+                            prevToken = token
+                        }
+                    } else {// Step 2.2: not part of titles and the first letter is lowercase
+                        suggestionList.add("Please capitalize the first letter of the: ${tokens.first()}")
+                    }
+
+                    // Update the adapter with the new suggestion list
+                    adapter.notifyDataSetChanged()
+                }// end of for (sentence in sentences)
+
+
+                val tokenArray = tokenize(text.toString().lowercase())
+                val getPattern = getPattern(tokenArray)
+                val closestPattern = findClosestPattern(getPattern)
+                val hint = rearrange(closestPattern, getPattern, tokenArray.toString())
+
+                if (hint != null) {
+                    suggestionList.add("Did you mean: " + hint.joinToString(separator = " "))
+                }
+
+                adapter.notifyDataSetChanged()
+            }
+
+
+        })
 
     }
+
+    // Function to get the closest word in wordList using Levenshtein distance
+    fun getClosestWord(word: String): String? {
+        var minDistance = Int.MAX_VALUE
+        var closestWord: String? = null
+
+        for (w in wordList) {
+            val distance = levenshteinDistance(word, w)
+            if (distance < minDistance) {
+                minDistance = distance
+                closestWord = w
+            }
+        }
+
+        return closestWord
+    }
+    fun levenshteinDistance(s: String, t: String): Int {
+        val m = s.length
+        val n = t.length
+        val d = Array(m + 1) { IntArray(n + 1) }
+
+        for (i in 0..m) {
+            d[i][0] = i
+        }
+
+        for (j in 0..n) {
+            d[0][j] = j
+        }
+
+        for (j in 1..n) {
+            for (i in 1..m) {
+                if (s[i - 1] == t[j - 1]) {
+                    d[i][j] = d[i - 1][j - 1]
+                } else {
+                    d[i][j] = 1 + minOf(
+                        d[i - 1][j],
+                        d[i][j - 1],
+                        d[i - 1][j - 1]
+                    )
+                }
+            }
+        }
+
+        return d[m][n]
+    }
+
+    // for rearrangement side in NLP
+    fun findClosestPattern(queryPattern: List<String>): List<String>? {
+        val posPattern = listOf(
+            listOf("infinitive", "noun"),
+            listOf("interrogative", "verb", "noun"),
+            listOf("noun", "verb", "adverb"),
+            listOf("noun", "conjunction", "noun"),
+            listOf("adjective", "adverb", "noun"),
+            listOf("imperative", "noun", "verb"),
+            listOf("adjective", "noun", "verb"),
+            listOf("verb", "preposition", "noun"),
+            listOf("adjective", "grammar", "noun"),
+            listOf("idiomatic", "noun", "verb"),
+            listOf("comparative", "adjective", "noun"),
+            listOf("exclamation", "interjection")
+        )
+        var closestPattern: List<String>? = null
+        var maxMatches = 0
+
+        val filteredPatterns = posPattern.filter { pattern -> pattern.size == queryPattern.size }
+
+        for (pattern in filteredPatterns) {
+            var matches = 0
+            val sortedQuery = queryPattern.sorted()
+            val sortedPattern = pattern.sorted()
+            for ((i, part) in sortedQuery.withIndex()) {
+                if (part == sortedPattern[i]) {
+                    matches++
+                }
+            }
+            if (matches > maxMatches) {
+                maxMatches = matches
+                closestPattern = pattern
+            }
+        }
+
+        return closestPattern
+    }
+    fun rearrange(patternToUse: List<String>?, queryPattern: List<String>?, text: String?): List<String> {
+        // Check for null values
+        if (patternToUse?.contains("unknown") == true || queryPattern?.contains("unknown") == true || text == null) {
+            println("One or more input variables are unknown. Process can't be done.")
+            return emptyList()
+        }
+
+        val tokenArray = tokenize(text)
+        // Check length of query_pattern and token_array
+        if (queryPattern != null) {
+            if (queryPattern.size != tokenArray.size) {
+                println("Length of query_pattern and token_array lists should be equal. Process can't be done.")
+                return emptyList()
+            }
+        }
+
+        val wordDict = queryPattern?.zip(tokenArray)?.toMap()
+        if (patternToUse != null) {
+            if (wordDict != null) {
+                return patternToUse.map { wordDict.getOrDefault(it, "") }
+            }
+        }
+
+        return emptyList()
+    }
+    fun tokenize(sentence: String): List<String> {
+        val regex = Regex("[^A-Za-z0-9ñÑ ]")
+        val sanitizedSentence = sentence.replace(regex, "")
+        return sanitizedSentence.split(" ")
+    }
+    fun getPattern(tokenArray: List<String>): List<String> {
+
+        val identifiedTypes = mutableListOf<String>()
+
+        val wordLists = mapOf("expression" to resources.openRawResource(R.raw.expression).bufferedReader().readLines(),
+            "exclamation" to resources.openRawResource(R.raw.exclamation).bufferedReader().readLines(),
+            "adverb" to resources.openRawResource(R.raw.adverb).bufferedReader().readLines(),
+            "prefix" to resources.openRawResource(R.raw.prefix).bufferedReader().readLines(),
+            "interrogative" to resources.openRawResource(R.raw.interrogative).bufferedReader().readLines(),
+            "verb" to resources.openRawResource(R.raw.verb).bufferedReader().readLines(),
+            "infinitive" to resources.openRawResource(R.raw.infinitive).bufferedReader().readLines(),
+            "idiomatic" to resources.openRawResource(R.raw.idiomatic).bufferedReader().readLines(),
+            "comparative" to resources.openRawResource(R.raw.comparative).bufferedReader().readLines(),
+            "colloquial" to resources.openRawResource(R.raw.colloquial).bufferedReader().readLines(),
+            "noun" to resources.openRawResource(R.raw.noun).bufferedReader().readLines(),
+            "grammar" to resources.openRawResource(R.raw.grammar).bufferedReader().readLines(),
+            "adjective" to resources.openRawResource(R.raw.adjective).bufferedReader().readLines(),
+            "preposition" to resources.openRawResource(R.raw.preposition).bufferedReader().readLines(),
+            "conjunction" to resources.openRawResource(R.raw.conjunction).bufferedReader().readLines(),
+            "interjection" to resources.openRawResource(R.raw.interjection).bufferedReader().readLines())
+
+        for (token in tokenArray) {
+            var found = false
+            for ((wordListName, wordList) in wordLists) {
+                if (token in wordList) {
+                    identifiedTypes.add(wordListName)
+                    found = true
+                    break
+                }
+            }
+            if (!found) {
+                println("Unknown")
+            }
+        }
+
+        return identifiedTypes
+    }
+
 
     fun openDrawer(drawer:DrawerLayout){
         drawer.openDrawer(GravityCompat.START)
